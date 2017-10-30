d579457e408f701007ba906ed777c0c625a27de9
Huda Ridwan <kerupuk.lovers@gmail.com>
2017-10-30 15:03:15 +0700

    display api_url from now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const API_URL = "https://backend-dfzefqaelu.now.sh/"
+
 class App extends Component {
   render() {
     return (
-      <div 
+      <div id = "container">
+        <p> {API_URL} </p>
+      </div>
     );
   }
 }

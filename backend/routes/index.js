6b0a4d4a67c845d2c59f13f1f7011c58a3439784
Huda Ridwan <kerupuk.lovers@gmail.com>
2017-10-30 14:38:05 +0700

    Add data.json for dummy data

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const DATA = require("./data.json")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/', (req, res, next) => {
+	res.send(DATA)
 });
 
 module.exports = router;

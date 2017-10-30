9f8288d6cda1248eb3a92ae346c1655462b05746
Huda Ridwan <kerupuk.lovers@gmail.com>
2017-10-30 15:05:54 +0700

    Add initialize constructor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const API_URL = "https://backend-dfzefqaelu.now.sh/"
+const API_URL = "https://backend-dfzefqaelu.now.sh"
 
 class App extends Component {
+  constructor (props) {
+  super(props)
+  this.state = {
+    data: []
+  }
+}
   render() {
     return (
       <div id = "container">

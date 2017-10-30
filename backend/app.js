04d7a6a6eb09944d8c1affadd03615b6a407afb6
Huda Ridwan <kerupuk.lovers@gmail.com>
2017-10-30 15:19:10 +0700

    install cors for backend

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const cors = require('cors');
 
 // ---------------------------------------------------------------------------------------------
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // --------------------------------------------------------------------------------------------
 
+app.use(cors());
 app.use('/', index);
 app.use('/users', users);
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
 
 const API_URL = "https://backend-dfzefqaelu.now.sh"
 
@@ -10,12 +9,24 @@ class App extends Component {
     data: []
   }
 }
+
+  async getInitialState() {
+    const RESPONSE = await fetch(API_URL)
+    const DATA = await RESPONSE.json()
+    console.log(DATA)
+    this.setState(state => {
+      return {
+        data: DATA
+      }
+    })
+  }
+
   render() {
     return (
       <div id = "container">
         <p> {API_URL} </p>
       </div>
-    );
+    )
   }
 }
 
